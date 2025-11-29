25234b7177e0cd453a13df1ed8275000ccd69283
Mahdi Anjidani <>
2025-11-29 13:02:18 +0330

    branch

--- 7/nil.go
+++ 7/nil.go
@@ -1,1 +1,40 @@
-package _
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func add(c chan int) {
+	sum := 0
+	t := time.NewTimer(time.Second * 2)
+	for {
+		select {
+		case input := <-c:
+			sum += input
+		case <-t.C:
+			c = nil
+			fmt.Println(sum)
+			wg.Done()
+		}
+	}
+}
+func send(c chan int) {
+	for {
+		c <- rand.Intn(69)
+	}
+}
+
+func main() {
+	c := make(chan int)
+	wg.Add(1)
+	go add(c)
+	go send(c)
+	time.Sleep(time.Second * 2)
+	wg.Wait()
+	fmt.Println("done")
+}
