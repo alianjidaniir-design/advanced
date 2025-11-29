a5489ccf2bf426e297758e623eb5de7969fe845a
Mahdi Anjidani <>
2025-11-29 12:35:01 +0330

    branch

--- 6/buf.go
+++ 6/buf.go
@@ -1,1 +1,29 @@
-package _
+package main
+
+import "fmt"
+
+func main() {
+
+	num := make(chan int, 5)
+	counter := 10
+	for i := 2; i < counter; i++ {
+		select {
+		case num <- i:
+			fmt.Println(1)
+		case num <- i * i:
+			fmt.Println("proccessing", i)
+		default:
+			fmt.Println("Not")
+		}
+	}
+	for {
+		select {
+		case num := <-num:
+			fmt.Println("@", num, " ")
+		default:
+			fmt.Println("default")
+			return
+		}
+	}
+
+}
