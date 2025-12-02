b2e6e3e515eaa72ae3fb689bd1fb231c14b60475
Mahdi Anjidani <>
2025-12-02 09:33:01 +0330

    branch

--- 13/context2.go
+++ 13/context2.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancelCause(ctx)
+	cancel(errors.New("Cancled By timeout"))
+	err := takingTooLong(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func takingTooLong(ctx context.Context) error {
+	select {
+	case <-time.After(3 * time.Second):
+		fmt.Println("Done")
+		return nil
+	case <-ctx.Done():
+		fmt.Println("Cancelled")
+		return ctx.Err()
+	}
+}
