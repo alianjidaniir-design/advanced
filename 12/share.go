13b2de72df9399a7450b2c874fdc67e328e95c96
Mahdi Anjidani <>
2025-12-02 07:48:42 +0330

    branch

---
+++ 12/share.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var readValue = make(chan int)
+var writeValue = make(chan int)
+
+func set(newValue int) {
+	writeValue <- newValue
+}
+
+func Read() int {
+	return <-readValue
+}
+
+func monitor() {
+	var value int
+	for {
+		select {
+		case newValue := <-writeValue:
+			value = newValue
+			fmt.Println(value)
+		case readValue <- value:
+		}
+	}
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("two")
+		return
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Going to create %d random numbers.\n", n)
+	go monitor()
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			set(rand.Intn(n * n))
+		}()
+	}
+	wg.Wait()
+	fmt.Println(Read())
+}
