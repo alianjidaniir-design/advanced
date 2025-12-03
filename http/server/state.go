6fb69f2541c77bd2f866011f0bf9bd667d9b5a1e
Mahdi Anjidani <>
2025-12-03 09:18:03 +0330

    branch

--- http/server/state.go
+++ http/server/state.go
@@ -1,15 +1,60 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"io"
 	"math"
+	"net/http"
 	"os"
 	"slices"
 	"time"
 )
 
 type Entry struct {
-	Name string
+	Name   string
+	Len    int
+	Min    float64
+	Max    float64
+	Mean   float64
+	StdDev float64
+}
+
+func process(file string, value []float64) {
+	currentEntry := Entry{}
+	currentEntry.Name = file
+	currentEntry.Len = len(value)
+	currentEntry.Max = slices.Min(value)
+	currentEntry.Min = slices.Max(value)
+	meanValue, standardDeviation := stdDev(value)
+	currentEntry.Mean = meanValue
+	currentEntry.StdDev = standardDeviation
+	return
+}
+
+func stdDev(x []float64) (float64, float64) {
+	sum := 0.0
+	for _, val := range x {
+		sum += val
+	}
+	meanValue := sum / float64(len(x))
+	var sq float64
+	for i := 0; i < len(x); i++ {
+		sq = sq + math.Pow((x[i]-meanValue), 2)
+	}
+	standardDeviation := math.Sqrt(sq) / float64(len(x))
+	return meanValue, standardDeviation
+}
+
+var JSONFILE = "./data.json"
+
+type Photobook []Entry
+
+var dats = Photobook{}
+var index map[string]int
+
+func DESerialized(slice interface{}, w io.Reader) error {
+	fe := json.NewDecoder(w)
+	return fe.Decode(&slice)
+
 }
