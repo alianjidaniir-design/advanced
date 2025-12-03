6ee144938d289082a427772c53d6a4e643cfe2ca
Mahdi Anjidani <>
2025-12-03 10:53:11 +0330

    branch

--- http/server/handlers.go
+++ http/server/handlers.go
@@ -14,7 +14,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 	w.WriteHeader(http.StatusOK)
 	body := "Hello World!"
-	fmt.Fprintln(w, body)
+	fmt.Fprintf(w, "%s", body)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,10 +32,30 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		Body := err.Error() + "\n"
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, Body)
+		fmt.Fprintf(w, "%s", Body)
 		return
 	}
 	body := dataset + "deleted!\n"
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, body)
+	fmt.Fprintf(w, "%s", body)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	body := list()
+	fmt.Fprintf(w, "%s", body)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	body := fmt.Sprintf("Total entries: %d\n", len(list()))
+	fmt.Fprintf(w, "%s", body)
+
+}
+
+func insertHandler(w http.ResponseWriter, r *http.Request) {
+	paramstr := strings.Split(r.URL.Path, "/")
+	fmt.Println("Path:", paramstr)
 }
