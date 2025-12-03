1020b690c4acc4724c7e7978b54582b21b397343
Mahdi Anjidani <>
2025-12-03 07:47:24 +0330

    branch

--- http/webserver.go
+++ http/webserver.go
@@ -1,1 +1,41 @@
-package http
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Printf("Served %s\n", r.Host)
+
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Format("Mon Jan 02 15:04:05 -0700 2006")
+	Body := "My time is: "
+	fmt.Fprintf(w, "<h1 align=\\\"center\\\">%s</h1>", Body)
+	fmt.Fprintf(w, "<h2 align=\\\"center\\\">%s</h2>", t)
+	fmt.Fprintf(w, "<h3 align=\\\"center\\\">%s</h3>", r.URL.Path)
+	fmt.Printf("Served time for: %s\n", r.Host)
+}
+
+func main() {
+	PORT := ":8001"
+	arg := os.Args
+	if len(arg) != 1 {
+		PORT = ":" + arg[1]
+	}
+	fmt.Println("Using port number: " + PORT)
+
+	http.HandleFunc("/time", timeHandler)
+	http.HandleFunc("/", myHandler)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
