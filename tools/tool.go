3559939e94829cfa607d562beac858bb716ef83f
Mahdi Anjidani <>
2025-12-09 09:43:43 +0330

    branch

--- tools/tool.go
+++ tools/tool.go
@@ -9,30 +9,51 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 2 {
-		fmt.Println("Not enough arguments")
+
+	if len(os.Args) == 1 {
+
+		fmt.Println("Not enough arguments!")
+
 		return
+
 	}
+
 	g := new(errgroup.Group)
 
 	for _, url := range os.Args[1:] {
+
 		url := url
+
 		g.Go(func() error {
+
 			resp, err := http.Get(url)
+
 			if err != nil {
+
 				return err
+
 			}
+
 			defer resp.Body.Close()
-			fmt.Println(url, "is OK")
+
+			fmt.Println(url, "is OK.")
+
 			return nil
 
 		})
-		err := g.wait
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
 
 	}
+
+	err := g.Wait()
+
+	if err != nil {
+
+		fmt.Println("Error:", err)
+
+		return
+
+	}
+
 	fmt.Println("Everything went fine!")
+
 }
